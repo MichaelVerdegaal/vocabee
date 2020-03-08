bfec6fdb36b78ffdeb7e9f4c5a22aeb4a1b9a091
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-03-08 22:42:42 +0100

    add basic navbar

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/vocab')
+def vocab():
+    return render_template('vocab.html')
