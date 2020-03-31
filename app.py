429f4196d0e32fbefa8bf365df115a612aa75e89
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-03-31 17:09:27 +0200

    add caching

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_caching import Cache
 from file_util import vocabulary
 
+config = {
+    "DEBUG": False,  # some Flask specific configs
+    "CACHE_TYPE": "simple",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 600
+}
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 
 @app.route('/')
