642735cd6e6d993b0507b45f87cbed7dceae4ffb
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-04-19 22:43:30 +0200

    Switch to sql connection

--- file_util.py
+++ file_util.py
@@ -1,55 +1,70 @@
-from openpyxl import load_workbook
 import json
 
-
-def get_excel_sheet():
-    """
-    Retrieves excel sheet from our vocabulary file. At the moment we know that we only have 1 sheet,
-    so we can just retrieve the first one.
-    :return: Excel sheet
-    """
-    workbook = load_workbook('vocab.xlsx', data_only=True)
-    sheet = workbook[workbook.sheetnames[0]]
-    return sheet
+from psycopg2 import connect
+from config import *
 
 
-def get_entries(sheet):
+def get_connection():
     """
-    Retrieves vocabulary entries split by level
-    :param sheet: Excel sheet from vocabulary file
-    :return: Vocabulary entries as dictionary
+    Get database connection object
+    :return: connection
     """
-    N1 = {"entries": []}
-    N2 = {"entries": []}
-    N3 = {"entries": []}
-    N4 = {"entries": []}
+    return connect(host=host, port=port, database=database, user=user, password=password)
+
+
+def get_cursor(connection):
+    """
+    Get database cursor object
+    :param connection: database connection object
+    :return: cursor
+    """
+    return connection.cursor()
+
+
+def get_all_vocab(cursor):
+    """
+    Fetches all vocabulary entries from the database
+    :param cursor: cursor object
+    :return: vocabulary
+    """
+    cursor.execute("SELECT * FROM vocabulary")
+    vocab = cursor.fetchall()
+
+    return vocab
+
+
+def process_vocabulary(vocabulary):
+    """
+    Processes vocabulary entries so they can be used for the tables
+    :param vocabulary: vocabulary results as list of tuples
+    :return: procssed vocabulary
+    """
     N5 = {"entries": []}
-    for row in sheet.iter_rows():
-        # Each row contains a Kanji, Hiragana, English, JLPT level and Example sentences in that order
-        # We put entries in a list because DataTables only accepts rows in this format
-        # Add clickable Jisho link
-        kanji = f'<a href="https://jisho.org/search/{e}" target="_blank">{e}</a>' if (e := row[0].value) else ""
-        hiragana = f'<a href="https://jisho.org/search/{row[1].value}" target="_blank">{row[1].value}</a>'
-        english = e if (e := row[2].value) else ""
-        examples = e if (e := row[4].value) else ""
-        entry = [hiragana,
-                 kanji,
-                 english,
-                 examples]
+    N4 = {"entries": []}
+    N3 = {"entries": []}
+    N2 = {"entries": []}
+    N1 = {"entries": []}
 
-        level = row[3].value
+    for row in vocabulary:
+        # Add clickable Jisho links
+        vocab_id = row[0]
+        kanji = f'<a href="https://jisho.org/search/{e}" target="_blank">{e}</a>' if (e := row[1]) else ""
+        hiragana = f'<a href="https://jisho.org/search/{row[2]}" target="_blank">{row[2]}</a>'
+        english = e if (e := row[3]) else ""
+        entry = [vocab_id, kanji, hiragana, english]
+
+        level = row[4]
         # Split by JLPT level for performance reasons
-        if level == "N1":
-            N1["entries"].append(entry)
-        elif level == "N2":
-            N2["entries"].append(entry)
-        elif level == "N3":
-            N3["entries"].append(entry)
+        if level == "N5":
+            N5["entries"].append(entry)
         elif level == "N4":
             N4["entries"].append(entry)
-        elif level == "N5":
-            N5["entries"].append(entry)
-
+        elif level == "N3":
+            N3["entries"].append(entry)
+        elif level == "N2":
+            N2["entries"].append(entry)
+        elif level == "N1":
+            N1["entries"].append(entry)
     return [N1, N2, N3, N4, N5]
 
 
@@ -58,11 +73,15 @@ def get_vocabulary():
     Master function to get vocabulary and serialize it
     :return: Vocabulary entries as dictionary
     """
-    sheet = get_excel_sheet()
-    vocabulary = get_entries(sheet)
-    for level in vocabulary:
+    connection = get_connection()
+    cursor = get_cursor(connection)
+    vocabulary = get_all_vocab(cursor)
+    cursor.close()
+
+    processed_vocabulary = process_vocabulary(vocabulary)
+    for level in processed_vocabulary:
         json.dumps(level)
-    return vocabulary
+    return processed_vocabulary
 
 
 vocabulary = get_vocabulary()
