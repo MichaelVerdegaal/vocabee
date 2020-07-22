20a8bb7e503a71cc3fe695afa48530c1e0dc66b3
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-07-22 13:43:30 +0200

    Move example data to script

--- vocabee/static/js/tables.js
+++ vocabee/static/js/tables.js
@@ -34,3 +34,33 @@ function createVocabTable() {
             }
         ]);
 }
+
+function fillExampleModal(data, kanji, hiragana) {
+    // Fill modal with examples
+    let modal_content = document.getElementById("vocab-modal-body");
+    modal_content.innerHTML = '';
+    let example_count = data.length;
+    if (example_count > 0) {
+        for (let e of data) {
+            let paragraph = document.createElement("p");
+            paragraph.innerHTML = `${e[1]}<br>${e[2]}<br>`;
+            modal_content.appendChild(paragraph);
+        }
+    } else {
+        let paragraph = document.createElement("p");
+        paragraph.innerHTML = `Sorry, no examples for this entry...`;
+        modal_content.appendChild(paragraph);
+    }
+
+    // Set modal title
+    let modal_title = document.getElementById("modalLargeLabel");
+    let example_text = "examples";
+    if (example_count === 1) {
+        example_text = "example";
+    }
+    if (typeof kanji !== 'undefined') {
+        modal_title.textContent = `Showing ${example_count} ${example_text} for ${kanji}/${hiragana}`;
+    } else {
+        modal_title.textContent = `Showing ${example_count} ${example_text} for ${hiragana}`;
+    }
+}
