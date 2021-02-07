5d0674b519c4516ae20c70a6c274a5ab0e1930ef
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-02-07 12:49:30 +0100

    Refresh connection when it times out

--- vocabee/config.py
+++ vocabee/config.py
@@ -9,4 +9,6 @@ CACHE_DEFAULT_TIMEOUT = 60
 # SQLAlchemy
 SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.getenv('user')}:{os.getenv('password')}@{os.getenv('host')}:3306/{os.getenv('database')}"
 SQLALCHEMY_ECHO = False
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_POOL_RECYCLE = 180
+SQLALCHEMY_POOL_SIZE = 20
