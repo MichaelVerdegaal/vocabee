c861b439bd693116569b7845c665b4640e851f28
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-07-04 22:13:01 +0200

    Add id to search matching

--- vocabee/util/vocabulary_util.py
+++ vocabee/util/vocabulary_util.py
@@ -29,23 +29,23 @@ def search_vocabulary(search_query, vocabulary_collection):
     :param vocabulary_collection: list of dicts of vocabulary entries to search through
     :return: dict of match and perfect match counts, perfect matches and fuzzy matches
     """
-    perfect_match_count = 0
-    match_count = 0
+    perfect_match_count = 1
+    match_count = 1
     perfect_matches = []
     matches = []
     for row in vocabulary_collection['entries']:
-        for col in ['kanji', 'kana', 'english']:
-            ratio = fuzz.partial_ratio(search_query, row[col])
+        for col in ['english', 'kana', 'kanji', 'id']:
+            ratio = fuzz.partial_ratio(search_query, str(row[col]))
             match_item = {'matched_on': col, 'fuzzy_ratio': ratio, 'match_data': row}
             if ratio == 100:
                 perfect_match_count += 1
                 perfect_matches.append(match_item)
+                break
             elif ratio >= 75:
                 match_count += 1
                 matches.append(match_item)
+                break
 
-    perfect_matches = sorted(perfect_matches, key=lambda k: k['fuzzy_ratio'], reverse=True)
-    matches = sorted(matches, key=lambda k: k['fuzzy_ratio'], reverse=True)
     return {'perfect_match_count': perfect_match_count,
             'match_count': match_count,
             'perfect_matches': perfect_matches,
