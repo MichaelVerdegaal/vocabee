c55185426623db6de2d996ab096f8da477ab2bb2
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-07-07 11:57:06 +0200

    Make database URI easier to read

--- vocabee/config.py
+++ vocabee/config.py
@@ -1,5 +1,12 @@
 import os
 
+# Environment variables
+db_user = os.getenv('db_user')
+db_password = os.getenv('db_password')
+db_host = os.getenv('db_host')
+db_port = os.getenv('db_port')
+db_name = os.getenv('db_name')
+
 # Project
 PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_FOLDER = os.path.join(PROJECT_FOLDER, 'vocabee')
@@ -12,7 +19,7 @@ DEBUG = False
 CACHE_DEFAULT_TIMEOUT = 60
 
 # SQLAlchemy
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.getenv('db_user')}:{os.getenv('db_password')}@{os.getenv('db_host')}:{os.getenv('db_port')}/{os.getenv('db_name')}"
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_POOL_RECYCLE = 180
