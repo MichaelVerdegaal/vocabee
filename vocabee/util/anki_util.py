6b81e770d78b161d6f0813139b218e1d96e4cb10
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-08-27 15:41:57 +0200

    Clean up anki util file
    Improve performance when excluding example sentences

--- vocabee/util/anki_util.py
+++ vocabee/util/anki_util.py
@@ -1,25 +1,29 @@
-import datetime
-
 import genanki
 
-vocabulary_model = genanki.Model(
-    # ID's need to be hardcoded due to anki requirements
-    1963760736,
-    'JP model',
-    fields=[
-        {'name': 'Kana'},
-        {'name': 'Kanji'},
-        {'name': 'English'},
-        {'name': 'Example'}
-    ],
-    templates=[
-        {
-            'name': 'Card',
-            'qfmt': '<h1>{{Kana}}{{Kanji}}</h1>',
-            'afmt': '{{FrontSide}}<hr id="answer"><h2>{{English}}</h2> {{Example}}',
-        },
-    ],
-    css="h1, h2, h3, h4, h5, h6, p {text-align: center;}")
+# ID's need to be hardcoded due to anki requirements
+ANKI_MODEL_ID = 1963760736
+ANKI_DECK_ID = 2076601992
+
+
+def create_model(include_examples=True):
+    fields = [{'name': 'Kana'},
+              {'name': 'Kanji'},
+              {'name': 'English'}]
+    template = [{'name': 'Card',
+                 'qfmt': '<h1>{{Kana}}{{Kanji}}</h1>',
+                 'afmt': '{{FrontSide}}<hr id="answer"><h2>{{English}}</h2> '}]
+
+    if include_examples:
+        fields.append({'name': 'Example'})
+        template[0]['afmt'] = '{{FrontSide}}<hr id="answer"><h2>{{English}}</h2> {{Example}}'
+
+    vocabulary_model = genanki.Model(
+        ANKI_MODEL_ID,
+        'JP model',
+        fields=fields,
+        templates=template,
+        css="h1, h2, h3, h4, h5, h6, p {text-align: center;}")
+    return vocabulary_model
 
 
 def create_example_note_string(example_list):
@@ -34,24 +38,6 @@ def create_example_note_string(example_list):
     return note_string
 
 
-def create_note(example_list, kana, kanji, english):
-    """
-    Creates an anki note (card)
-    :param example_list: list of example sentences
-    :param kana: kana text
-    :param kanji: kanji text
-    :param english: english text
-    :return: anki note
-    """
-    example_string = ''
-    if example_list:
-        example_string = create_example_note_string(example_list[:3])
-        example_string = f'<hr><br><h2>Example usage</h2>{example_string}'
-
-    kanji = f"/{kanji}" if kanji else ""
-    return genanki.Note(model=vocabulary_model, fields=[kana, kanji, english, example_string])
-
-
 def create_deck(level):
     """
     Creates an anki deck
@@ -59,36 +45,41 @@ def create_deck(level):
     :return: anki deck
     """
     # ID's need to be hardcoded due to anki requirements
-    deck_id = 2076601991
-    my_deck = genanki.Deck(deck_id=deck_id,
+    my_deck = genanki.Deck(deck_id=ANKI_DECK_ID,
                            name=f'Vocabee level {level}',
                            description='<h4 style="text-align: center">Japanese vocabulary deck from vocabee.xyz</h4>')
     return my_deck
 
 
-def fill_deck(vocabulary_list, deck, include_examples):
+def fill_deck(vocabulary_list, deck, model, include_examples):
     """
     Creates a notelist and add it to the deck
     :param vocabulary_list: vocabulary list
     :param deck: anki deck
     """
-    # TODO: Redo include_examples to be cleaner
-    if include_examples:
-        notelist = [create_note(v.examples, v.kana, v.kanji, v.english) for v in vocabulary_list]
-    else:
-        notelist = [create_note(None, v.kana, v.kanji, v.english) for v in vocabulary_list]
+
+    def create_note(entry):
+        """
+        Creates an anki note (card)
+        :param entry: vocabulary entry
+        :return: anki note
+        """
+        kana = entry.kana
+        english = entry.english
+        kanji = f"/{entry.kanji}" if entry.kanji else ""
+
+        fields = [kana, kanji, english]
+
+        if include_examples:
+            example_list = entry.examples
+            example_string = create_example_note_string(example_list[:3])
+            example_string = f'<hr><br><h2>Example usage</h2>{example_string}'
+            fields.append(example_string)
+
+        return genanki.Note(model=model, fields=fields)
+
+    notelist = [create_note(v) for v in vocabulary_list]
     deck.notes = notelist
-
-
-def write_deck(deck, filename):
-    """
-    Writes an anki deck to a file at the root directory of the project
-    :param deck: anki deck
-    :param filename: filename of deck
-    :return: name of the generated file
-    """
-    genanki.Package(deck).write_to_file(filename)
-    return filename
 
 
 def create_deck_by_level(vocabulary, level, filename, include_examples):
@@ -97,8 +88,10 @@ def create_deck_by_level(vocabulary, level, filename, include_examples):
     :param vocabulary: vocabulary list
     :param level: JLPT vocabulary level
     :param filename: filename of deck
+    :param include_examples: whether to include examples in the notes
     :return: name of the generated file
     """
     new_deck = create_deck(level)
-    fill_deck(vocabulary, new_deck, include_examples)
-    write_deck(new_deck, filename)
+    model = create_model(include_examples)
+    fill_deck(vocabulary, new_deck, model, include_examples)
+    genanki.Package(new_deck).write_to_file(filename)
